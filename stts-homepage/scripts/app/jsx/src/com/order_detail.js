165cd2a15986fc3f14d53a75e2aad39bf1f598ec
wangyixian <ouitiken@gmail.com>
2016-10-26 03:51:13 +0800

    订单详细

--- stts-homepage/scripts/app/jsx/src/com/order_detail.js
+++ stts-homepage/scripts/app/jsx/src/com/order_detail.js
@@ -44,28 +44,52 @@ var Order = React.createClass({
         return (
             <div>
                 <OrderProcess step="1"/>
+                <h3>等待服务机构受理中，倒计时还剩 5天。</h3>
+                <div className="text-center mb-10">
+                    <button className="btn btn-default">交易撤消/退券/退券撤消/退单/退单撤消</button>&nbsp;&nbsp;
+                    <button className="btn btn-default">付券</button>&nbsp;&nbsp;
+                    <button className="btn btn-default">确认完成</button>&nbsp;&nbsp;
+                    <button className="btn btn-default">评价</button>
+                </div>
                 <div className="panel panel-info">
-                    <div className="panel-heading">订单信息</div>
+                    <div className="panel-heading">
+                        订单信息&nbsp;&nbsp;订单号：201601011800001&nbsp;&nbsp;状态：申请服务&nbsp;&nbsp;
+                        <a href="#" className="pull-right">查看退款/退单信息</a>
+                    </div>
                     <div className="panel-body">
                         <div className="col-sm-4">
-                            <p>服务产品：<a href="#">财务审计（快照）</a></p>
+                            <p>服务产品名：<a href="#">xxx 服务（快照）</a></p>
 
                             <p>申请时间：2016-01-01 18:00:00</p>
 
-                            <p>服务周期：2016-06-05 ～ 2016-09-30</p>
+                            <p>附加条款：XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
                         </div>
                         <div className="col-sm-4">
-                            <p>服务价格：1.2 万元</p>
+                            <p>服务分类：人才引培-高层培训</p>
 
+                            <p>服务周期：2016-06-05 ～ 2016-09-30</p>
+
+                            <p>附件：<img src="#"/>&nbsp;<img src="#"/>&nbsp;<img src="#"/></p>
+
+                        </div>
+                        <div className="col-sm-4">
                             <p>实际费用：10000 元</p>
 
-                            <p>服务券额：3000 元</p>
-                        </div>
-                        <div className="col-sm-4">
-                            <p>状态：待受理</p>
+                            <p>已支付服务券：3000 元</p>
 
                             <p><a href="#">阅览合同</a></p>
                         </div>
+                    </div>
+                </div>
+                <div className="panel panel-info">
+                    <div className="panel-heading">付券记录</div>
+                    <div className="panel-body">
+                            2016-01-01 18:00:00 买家付款 券 500元<br/>
+                            2016-02-01 18:00:00 买家付款 券 500元<br/>
+                            2016-02-02 18:00:00 买家申请退款 券 500元<br/>
+                            2016-02-02 18:00:00 卖家退款 券 500元<br/>
+                            2016-03-02 18:00:00 买家申请退单<br/>
+                            2016-03-03 18:00:00 卖家同意退单
                     </div>
                 </div>
                 <div className="panel panel-info">
@@ -104,12 +128,12 @@ var Order = React.createClass({
                                     xxxxxxxxxxxxxxxxxxxxxxx</p>
                             </div>
                             <div className="col-sm-3">
-                                <p>类型：小升规</p>
+                                <p>类型：股份制企业</p>
 
-                                <p>负责人：金喜善</p>
+                                <p>联系电话：1234323456</p>
                             </div>
                             <div className="col-sm-3">
-                                <p>联系电话：1234323456</p>
+                                <p>负责人：金喜善</p>
                             </div>
                         </div>
                     </div>
@@ -121,60 +145,60 @@ var Order = React.createClass({
                             <div className="col-sm-6">
                                 <h3>对服务机构的评价</h3>
 
-                                <p>综合评价：
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
+                                <p>机构综合评分：
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
                                 </p>
 
                                 <p>服务态度：
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
                                 </p>
 
                                 <p>专业水平：
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
                                 </p>
 
-                                <p>描述属实：
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
+                                <p>描述是否相等：
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
                                 </p>
 
-                                <p>补充说明：xxxxxxxxxxxxxxxxxxxxxxx</p>
+                                <p>说明：本次服务非常满意</p>
                             </div>
                             <div className="col-sm-6">
                                 <h3>对企业的评价</h3>
 
-                                <p>综合评价：
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
+                                <p>企业综合评分：
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
                                 </p>
 
-                                <p>付款及时：
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
-                                    <i className="fa fa-star text-gold" aria-hidden="true"></i>
+                                <p>付款是否及时：
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
+                                    <i className="fa fa-star text-danger" aria-hidden="true"></i>
                                 </p>
 
-                                <p>补充说明：xxxxxxxxxxxxxxxxxxxxxxx</p>
+                                <p>说明：本次服务非常满意</p>
                             </div>
                         </div>
                     </div>
--- stts-homepage/scripts/app/jsx/src/com/order_list.js
+++ stts-homepage/scripts/app/jsx/src/com/order_list.js
@@ -38,20 +38,23 @@ var OrderList = React.createClass({
                             <div className="panel panel-default">
                                 <div className="panel-heading">订单一览</div>
                                 <div className="panel-body">
-                                    <div className="row overflow-hidden mb-10">
-                                        <div className="col-sm-6">
-                                            <div className="input-group">
-                                                <input type="text" className="form-control"/>
-                                              <span className="input-group-btn">
-                                                <button className="btn btn-default" type="button">搜&nbsp;索</button>
-                                              </span>
-                                            </div>
+                                    <div className="form-inline mb-10">
+                                        <div className="form-group">
+                                            <label>服务名称</label> &nbsp;
+                                            <input type="text" className="form-control"/>
                                         </div>
-                                        <div className="col-sm-6">
-                                            <button id="btnCondition" className="btn btn-default"
-                                                    onClick={this.handleCondition}>更多筛选条件
-                                            </button>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <div className="form-group">
+                                            <label>订单时间</label>&nbsp;
+                                            <select className="form-control">
+                                                <option>1个月</option>
+                                            </select>
                                         </div>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <button type="button" className="btn btn-default">搜&nbsp;索</button>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <a id="btnCondition" className="btn" onClick={this.handleCondition}>更多筛选条件
+                                        </a>
                                     </div>
                                     <div id="moreConditionWrap" style={{display:"none"}}
                                          className="form-horizontal overflow-hidden">
@@ -59,7 +62,7 @@ var OrderList = React.createClass({
                                             <div className="col-sm-4">
                                                 <div className="form-group">
                                                     <div className="control-label col-sm-4">
-                                                        <label>大分类</label>
+                                                        <label>服务大类</label>
                                                     </div>
                                                     <div className="col-sm-8">
                                                         <select className="form-control">
@@ -70,7 +73,7 @@ var OrderList = React.createClass({
                                             <div className="col-sm-4">
                                                 <div className="form-group">
                                                     <div className="control-label col-sm-4">
-                                                        <label>小分类</label>
+                                                        <label>服务小类</label>
                                                     </div>
                                                     <div className="col-sm-8">
                                                         <select className="form-control">
@@ -85,41 +88,6 @@ var OrderList = React.createClass({
                                                     </div>
                                                     <div className="col-sm-8">
                                                         <input type="text" className="form-control"/>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-sm-4">
-                                                <div className="form-group">
-                                                    <div className="control-label col-sm-4">
-                                                        <label>评价状态</label>
-                                                    </div>
-                                                    <div className="col-sm-8">
-                                                        <select className="form-control">
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-sm-4">
-                                                <div className="form-group">
-                                                    <div className="control-label col-sm-4">
-                                                        <label>交易状态</label>
-                                                    </div>
-                                                    <div className="col-sm-8">
-                                                        <select className="form-control">
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-sm-4">
-                                                <div className="form-group">
-                                                    <div className="control-label col-sm-4">
-                                                        <label>订单时间</label>
-                                                    </div>
-                                                    <div className="col-sm-8">
-                                                        <select className="form-control">
-                                                        </select>
                                                     </div>
                                                 </div>
                                             </div>
@@ -143,15 +111,12 @@ var OrderList = React.createClass({
                                             <div className="col-sm-4">
                                                 <div className="form-group">
                                                     <div className="control-label col-sm-4">
-                                                        <label>成交时间</label>
+                                                        <label>交易状态</label>
                                                     </div>
                                                     <div className="col-sm-8">
-                                                        <div className="input-group">
-                                                            <input type="text" className="form-control"/>
-                                                        <span
-                                                            className="input-group-addon bg-clear border-clear">~</span>
-                                                            <input type="text" className="form-control"/>
-                                                        </div>
+                                                        <select className="form-control">
+                                                            <option></option>
+                                                        </select>
                                                     </div>
                                                 </div>
                                             </div>
@@ -161,6 +126,17 @@ var OrderList = React.createClass({
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="sort-row overflow-hidden">
+                                        <div className="sort-inner">
+                                            <ul className="sorts">
+                                                <li className="sort active"><a href="#">默认</a></li>
+                                                <li className="sort"><a href="#">订单时间</a></li>
+                                                <li className="sort"><a href="#">服务分类</a></li>
+                                                <li className="sort"><a href="#">价格</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
                                     <OrderItem/>
                                     <OrderItem/>
                                     <OrderItem/>
@@ -172,6 +148,8 @@ var OrderList = React.createClass({
                         </div>
                     </div>
                 </div>
+
+                <Footer/>
             </div>
         );
     }
@@ -184,28 +162,40 @@ var OrderItem = React.createClass({
                 <div className="panel-heading">2016-01-01 18:00:00&nbsp;&nbsp;订单号：201601010001</div>
                 <div className="panel-body">
                     <div className="row">
-                        <div className="col-sm-2">
-                            <img src="../../img/sample/service_small.jpg"/>
-                        </div>
-                        <div className="col-sm-3 border-right-dotted">
-                            <div><a href="#">财务审计</a></div>
-                            <br/>
-                            <div><a href="#">中国出口信用保险公司</a></div>
+                        <div className="col-sm-4 border-right-dotted">
+                            <div className="col-sm-6">
+                                <img src="../../img/sample/service_small.jpg"/>
+                            </div>
+                            <div className="col-sm-6">
+                                <div><a href="#">服务名称</a></div>
+                                <br/>
+
+                                <div><a href="#">服务机构名称</a></div>
+                            </div>
                         </div>
                         <div className="col-sm-2 border-right-dotted">
-                            <div>总金额：2000元</div>
+                            <div>成交金额：2000元</div>
                             <br/>
-                            <div>服务券：1000元</div>
+
+                            <div>已付服务券：1000元</div>
                         </div>
-                        <div className="col-sm-2">
+                        <div className="col-sm-2 border-right-dotted">
                             <div>状态：服务中</div>
                             <br/>
-                            <div>倒计时：5天</div>
+
+                            <div>人才引培-高层培训</div>
                         </div>
-                        <div className="col-sm-3 border-left-dotted">
-                            <div><a href="#">取消</a> | <a href="#">确认</a></div>
+                        <div className="col-sm-2">
+                            <div>服务联系人：李蓉</div>
                             <br/>
-                            <div><a href="#">退款</a> | <a href="order_detail.html">详细</a></div>
+
+                            <div>服务联系电话：89991234</div>
+                        </div>
+                        <div className="col-sm-2 border-left-dotted">
+                            <div><a href="#">交易撤消/退券/退券撤消/退单/退单撤消</a> | <a href="#">付券</a></div>
+                            <br/>
+
+                            <div><a href="#">确认完成</a> | <a href="#">评价</a> | <a href="order_detail.html">详细</a></div>
                         </div>
                     </div>
                 </div>
