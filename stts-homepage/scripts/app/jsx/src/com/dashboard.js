f0b51f162ca061c4c662dbe92ba40394e2e5ac45
wangyixian <ouitiken@gmail.com>
2016-10-26 02:49:37 +0800

    企业我的平台页面

--- stts-homepage/scripts/app/jsx/src/com/dashboard.js
+++ stts-homepage/scripts/app/jsx/src/com/dashboard.js
@@ -12,24 +12,47 @@ var Dashboard = React.createClass({
                         <li className='active'>我的平台</li>
                     </ol>
                     <div className="text-center">
-                        <div className="row">
+                        <div className="row mb-20">
                             <div className="col-sm-6">
                                 <img src="../../img/com/dashboard_1.png"/>
+
+                                <p>过期预警：2000 元服务券即将在2006年12月31日过期</p>
                             </div>
                             <div className="col-sm-6">
                                 <img src="../../img/com/dashboard_2.png"/>
                             </div>
                         </div>
-                        <br/>
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <img src="../../img/com/dashboard_3.png"/>
-                            </div>
-                            <div className="col-sm-6">
-                                <img src="../../img/com/dashboard_4.png"/>
+
+                        <div className="mb-20">
+                            <div className="panel panel-info">
+                                <div className="panel-heading">最新消息</div>
+                                <div className="panel-body">
+
+                                    <NoticeItem/>
+                                    <NoticeItem/>
+                                    <NoticeItem/>
+                                    <NoticeItem/>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <Footer/>
+            </div>
+        );
+    }
+});
+
+var NoticeItem = React.createClass({
+    render: function () {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">2016-01-01 18:18:10</div>
+                <div className="panel-body">
+                    <div>系统维护通知</div>
+                    <hr/>
+                    <div>2016-01-01 23:00:00 系统进入升级维护，请务进行敏感操作。</div>
                 </div>
             </div>
         );
--- stts-homepage/scripts/app/jsx/src/com/widgets/header.js
+++ stts-homepage/scripts/app/jsx/src/com/widgets/header.js
@@ -10,15 +10,17 @@ var Header = React.createClass({
         return (
             <div className="header-wrap">
                 <div className="container">
+                    <h2>
+                        <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+                        &nbsp;小微企业创业创新服务券网上平台&nbsp;
+                        <span className="service-icon">企业机构</span>
+                    </h2>
                     <div className="menu-main">
                         <ul className="clearfix">
-                            <li className="title">
-                                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-                                &nbsp;小微企业创业创新服务券网上平台&nbsp;
-                                <span className="service-icon">企业机构</span>
-                            </li>
                             <li id="mainMenuDashboard"><a href="dashboard.html">我的平台</a></li>
                             <li id="mainMenuOrder"><a href="order_list.html">我的订单</a></li>
+                            <li id="mainMenuEvaluate"><a href="order_list.html">我的评价</a></li>
+                            <li id="mainMenuMessage"><a href="order_list.html">我的消息</a></li>
                             <li id="mainMenuCompany"><a href="company.html">资质审核</a></li>
                             <li id="mainMenuCoupon"><a href="coupon.html">服务券</a></li>
                         </ul>
